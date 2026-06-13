ca4dee18d1668ee9030c22eb48754d557b37d8d3
kko20s25095-ui <kko20_s25095@gclass.ice.go.kr>
2026-06-13 15:04:44 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+package Áö·ÚÃ£±â;
+
+public class Main {
+	public static void main(String[] args) {
+		new MineSweeper();
+	}
+}
--- MineSweeper.java
+++ MineSweeper.java
@@ -1,4 +1,4 @@
-package 지뢰게임;
+package 지뢰찾기;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -19,179 +19,208 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
-
-public class MineSweeper extends JFrame {
-
-	private JPanel contentPane;
-	private JTextField txtSize;
-	buttonCell[][] cells; //셀로 나누어서 지뢰찾기 버튼을 만드는 것(배열)
-	int size, bomCnt; //폭탄의 개수 : bomCnt
-	private JButton btnRun;
-	private JPanel cp;
+class MineSweeper extends JFrame {
+	JPanel jp;
+	JTextField txtSize;
+	ButtonCell[][] cells; 
+	int size, bombCnt; //bomCnt는 지뢰를 매설할 개수임
+	JButton btnRun;
+	JPanel cp;
 	
-
+	boolean isOver;
+	
 	public MineSweeper() {
+		setTitle("\uC9C0\uB8B0\uAC8C\uC784");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(600, 600);
-		setLocationRelativeTo(null);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setBounds(100, 100, 626, 583);
+		jp = new JPanel();
+		jp.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
-		
-		JPanel jp = new JPanel();
-		contentPane.add(jp);
+		setContentPane(jp);
 		jp.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
-		jp.add(panel, BorderLayout.NORTH);
+		JPanel np = new JPanel();
+		jp.add(np, BorderLayout.NORTH);
 		
 		txtSize = new JTextField();
 		txtSize.setHorizontalAlignment(SwingConstants.CENTER);
-		panel.add(txtSize);
+		np.add(txtSize);
 		txtSize.setColumns(10);
 		
 		btnRun = new JButton("\uC2E4\uD589");
 		btnRun.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				cp.removeAll();
 				
-				size = Integer.parseInt(txtSize.getText()); //문자열은 객체이기 때문에 강제 형변환이 안된다.고로 Integer클래스의 파스를 가져옴
-				JOptionPane.showMessageDialog(null, size + " x " + size + "지뢰 찾기 생성 완료"); //나중에 지울 것
-				
+				size = Integer.parseInt(txtSize.getText());
+				cells = new ButtonCell[size][size]; //모든 버튼을 제어하는 변수
 				cp.setLayout(new GridLayout(0, size));
-				
-				cells = new buttonCell[size][size]; 
-				cp.removeAll(); //새로 지뢰찾기를 만들 때 예전 지뢰가 남지 않도록 해서 새로우 지뢰를 다시 만들도록 함
-				
-				
 				
 				//지뢰판 생성
 				for (int i = 0; i < size; i++) {
 					for (int j = 0; j < size; j++) {
-						cells[i][j] = new buttonCell(i, j);
+						cells[i][j] = new ButtonCell(i,j);
 						cp.add(cells[i][j]);
 					}
 				}
+				
 				cp.repaint();
 				cp.revalidate();
 				
-				//지뢰 설치 : 난수 => 갯수나 위치는 무작위 설치
-				bomCnt = (int)(size*size*0.1);
+				//지뢰 설치: 갯수나 위치는 무작위 설치
+				bombCnt = (int)(size*size*0.1);
+				
 				boolean[] nums = new boolean[size*size];
-				for (int i = 0; i < bomCnt; i++) {
-					// [수정] 줄 끝 유령 공백 제거
-					nums[i] = true; 
+				for (int i = 0; i < bombCnt; i++) {
+					nums[i] = true;
 				}
-				
-				//셔플 부분
+
 				Random rnd = new Random();
-				int a,b;
+				int a, b;
 				boolean c;
-				
 				for (int i = 0; i < size*size*size; i++) {
-					a = rnd.nextInt(size * size);
-					b = rnd.nextInt(size * size);
-					
-					c = nums[a]; nums[a] = nums[b]; nums[b] = c;
+					a=rnd.nextInt(size*size);
+					b=rnd.nextInt(size*size);
+					c=nums[a]; nums[a]=nums[b]; nums[b]=c;
 				}
-				
+			
+				// 1차적으로 모든 셀에 지뢰 상태를 먼저 주입합니다.
 				for (int i = 0; i < size*size; i++) {
 					cells[i/size][i%size].isBomb = nums[i];
-//					if (nums[i] == true) {
-//						cells[i/size][i%size].setText("\u004d"); //체크용
-//					}
 				}
 				
-				
-				
-				//셀마다 주변 8방의 지뢰 갯수 카운트
-				
-				
+				// 검증 및 수정 완료: 개별 try-catch 구조를 통해 8방향을 독립적으로 완벽히 카운트한다.
+				for (int i = 0; i < size*size; i++) {
+					int row = i/size , col = i % size;
+					
+					// 내가 지뢰가 아닐 경우에만 주변 지뢰 개수를 센다.
+					if (cells[row][col].isBomb == false) { 
+						
+						// 1. 북서 (위 왼쪽)
+						try {
+							if (row - 1 >= 0 && col - 1 >= 0 && cells[row-1][col-1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 2. 북 (위)
+						try {
+							if (row - 1 >= 0 && cells[row-1][col].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 3. 북동 (위 오른쪽)
+						try {
+							if (row - 1 >= 0 && col + 1 < size && cells[row-1][col+1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 4. 서 (왼쪽)
+						try {
+							if (col - 1 >= 0 && cells[row][col-1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 5. 동 (오른쪽)
+						try {
+							if (col + 1 < size && cells[row][col+1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 6. 남서 (아래 왼쪽)
+						try {
+							if (row + 1 < size && col - 1 >= 0 && cells[row+1][col-1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 7. 남 (아래)
+						try {
+							if (row + 1 < size && cells[row+1][col].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+
+						// 8. 남동 (아래 오른쪽)
+						try {
+							if (row + 1 < size && col + 1 < size && cells[row+1][col+1].isBomb) cells[row][col].surBomCnt++;
+						} catch (Exception ex) {}
+					}
+				}
 			}
 		});
-		panel.add(btnRun);
+		np.add(btnRun);
 		
 		cp = new JPanel();
 		jp.add(cp, BorderLayout.CENTER);
 		
-		setVisible(true); 
+		setVisible(true);
 	}
-	
-	class buttonCell extends JButton {
-		int r,c;
-		boolean isBomb; 
-		boolean isClicked = false;
-		boolean isRightClicked = false;
-		boolean isOval = false;
+
+	class ButtonCell extends JButton {
+		int surBomCnt; //주변에 폭탄이 몇 개인지 표시하는 변수
+		int r, c;
+		boolean isBomb, isClicked, isRightClicked;
 		
-		public buttonCell(int i, int j) { //버튼에 대한 정보를 입력받는 생성자
-			r = i; 
-			c = j;
+		public ButtonCell(int i, int j) {
+			r=i; c=j;
 			
 			this.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mousePressed(MouseEvent e) {
-					super.mousePressed(e);
-					
-					// 이미 클릭되어 열린 셀이거나 게임 오버 상태면 무시
-					if (isClicked) return;
-					
-					if (e.getButton() == MouseEvent.BUTTON1) { // 좌클릭
-						if (isRightClicked) return; 
-						
+					// 깃발(우클릭)이 꽂혀있지 않고, 이미 클릭된 상태가 아닐 때만 좌클릭 작동
+					if(e.getButton()==MouseEvent.BUTTON1 && !isRightClicked && !isClicked) {
 						isClicked = true;
-						repaint();
 						
-						if (isBomb) {
-							JOptionPane.showMessageDialog(null, "지뢰를 밟으셨습니다. 게임 종료!");
-							System.exit(0);
+						// 무한 루프 방지를 위해 paintComponent에 있던 버튼 상태 변경 코드를 이쪽으로 이동
+						setBorderPainted(false);
+						setContentAreaFilled(false);
+						setEnabled(false);
+						
+						if (isBomb == true) { 
+							repaint(); // 지뢰 아이콘을 그리도록 즉시 리페인트 호출
+							JOptionPane.showMessageDialog(null, "지뢰를 밟았어요. 게임 끝!");
+							isOver = true;
+							return;
 						}
 					}
-					else if (e.getButton() == MouseEvent.BUTTON3) { // 우클릭
-						isRightClicked = !isRightClicked;
-						repaint();
+					else if(e.getButton()==MouseEvent.BUTTON3) {
+						if(!isClicked) {
+							isRightClicked = !isRightClicked;
+							
+							// 우클릭 상태에 따른 버튼 속성을 여기서 한 번만 제어 (무한 루프 방지)
+							if(isRightClicked) {
+								setBorderPainted(true);
+								setContentAreaFilled(true);
+							}
+						}
 					}
+						
+					repaint();
 				}
 			});
 		}
 		
-		
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+
+			if(isClicked==false && isRightClicked==false) return;
 			
-			if (isClicked == false && isRightClicked == false) {
-				return;
-			}
-			
-			// 오른쪽 버튼을 누르면 깃발을 꽂는다. (회수하면 알아서 안 그려짐)
-			if (isRightClicked) {
-				this.setBorderPainted(true);
-				this.setContentAreaFilled(true);
-				this.setEnabled(true);
-				
+			//오른쪽 버튼 클릭함: 깃발을 꽂거나 제거한다. 토글 기능
+			if(isRightClicked) {
 				g.setColor(Color.RED);
 				g.setFont(new Font("Wingdings", 0, 30));
-				g.drawString("\uf50d", 15, 35);
+				g.drawString("\uf050", 15, 35);
+			} 
+			
+			// ★ 수정: 지뢰가 없는 칸을 클릭했을 때 주변 지뢰 개수가 1개 이상이면 파란색 숫자로 선명하게 그림
+			if(isClicked==true && isBomb==false) {
+				if (surBomCnt > 0) {
+					g.setColor(Color.BLUE);
+					g.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+					g.drawString(String.valueOf(surBomCnt), 20, 35);
+				} else {
+					g.drawString("", 15, 35);
+				}
 			}
 			
-			// 폭탄이 터졌을 경우 (좌클릭 시 작동)
-			if (isClicked == true && isBomb == true) {
-				
-				this.setBorderPainted(false);
-				this.setContentAreaFilled(false);
-				this.setEnabled(false);
-				isOval = true;
-				
-				g.setColor(Color.BLACK);
+			//클릭함. 지뢰가 있음. 게임 끝!
+			if(isClicked==true && isBomb==true) {
+				g.setColor(Color.RED);
 				g.setFont(new Font("Wingdings", 0, 30));
 				g.drawString("\uf04d", 15, 35);
-				
 			}
-			
-			
 		}
 	}
-
 }
